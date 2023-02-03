d83f87ba1c53f3f8726aeebaafc0e2fe42589737
Nicholas <nicholas.stevenson18@auhsdschools.org>
2023-02-03 10:53:46 -0600

    setting up mysql

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require('express')
+const mysql = require('mysql')
+const app = express();
+const port = 3000;
+
+const dbuser = 'schedule_handler'
+const dbpass = 'jW^71*01dZuy'
+
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: dbuser,
+    password: dbpass,
+    database: 'schedule_app'
+  })
+
+connection.connect();
 
 app.use(express.static("public"));
 
