145ad40072153219c9f088890bd311f56edaa00d
Nicholas <nicholas.stevenson18@auhsdschools.org>
2023-02-03 09:26:39 -0600

    fixed static file serving in express

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-app.use(express.static("static"));
+app.use(express.static("public"));
 
 app.get('/', (req, res) =>
 {
-    res.send('index.html')
+    res.send("index.html");
 })
-
-app.get('/schedule?', (req, res) =>
-{
-    res.send('schedule.html')
-})
-  
 app.listen(port, () =>
 {
     console.log(`scheduler listening on port ${port}`)
